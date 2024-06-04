55e3b6d468c24179786f781ce41e3f787114c9d1
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-06-03 23:42:40 -0400

    Create grounds_crew app and User model

---
+++ sgtbackend/grounds_crew/models/user.py
@@ -0,0 +1,25 @@
+from django.db import models
+import uuid
+from django.utils import timezone
+
+class User(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    email = models.EmailField(max_length=40, unique=True)
+    username = models.CharField(max_length=16, unique=True)
+    password = models.CharField(max_length=255)
+    f_name = models.CharField(max_length=16)
+    l_name = models.CharField(max_length=16)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        db_table = 'user'
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created_at = timezone.now()
+        self.updated_at = timezone.now()
+        return super(User, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
