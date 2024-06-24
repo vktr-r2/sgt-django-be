622559290f83feaa5c1fad3b1e9d40f02e733460
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-06-24 01:17:35 -0400

    Configure testing

--- sgtbackend/rough_draft/apps.py
+++ sgtbackend/rough_draft/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RoughPicksConfig(AppConfig):
+class RoughDraftConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "rough_picks"
+    name = "rough_draft"
