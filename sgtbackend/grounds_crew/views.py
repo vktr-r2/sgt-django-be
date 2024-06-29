2f70ef26ed9c6ee325384998772b1ad852c16bea
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-06-28 23:36:38 -0400

    Add test url path/view/template

--- sgtbackend/grounds_crew/views.py
+++ sgtbackend/grounds_crew/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_view(request):
+    # Your view logic here
+    context = {
+        'data_to_pass_to_template': 'Hello from your view!',
+    }
+    return render(request, 'login.html', context)
