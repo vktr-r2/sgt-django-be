d51b6fffe981f73d1b2eba865148128ef968a60f
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-06-30 16:40:25 -0400

    Add test view and template

--- sgtbackend/grounds_crew/views.py
+++ sgtbackend/grounds_crew/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from call_caddy.services.schedule_call import ScheduleCallService
 
 # Create your views here.
 
 def login_view(request):
     # Your view logic here
+
+    service_instance = ScheduleCallService()
+    json_string = service_instance.get_schedule(org_id="1")
     context = {
-        'data_to_pass_to_template': 'Hello from your view!',
-    }
+        'schedule_data': json_string
+        }
     return render(request, 'login.html', context)
