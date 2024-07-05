a206189bf02c643ad8ce54bba095a513d4691adf
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-07-04 22:46:13 -0400

    Add format field and update default values

--- sgtbackend/call_caddy/models/tournament.py
+++ sgtbackend/call_caddy/models/tournament.py
@@ -1,30 +1,23 @@
 from django.db import models
-from django.utils import timezone
-
 
 class Tournament(models.Model):
     source_id = models.CharField(max_length=10)
     name = models.CharField(max_length=100)
-    year = models.CharField(max_length=4)
-    golf_course = models.CharField(max_length=100)
-    location = models.CharField(max_length=255)
-    par = models.IntegerField(null=True, blank=True)
+    year = models.IntegerField()
+    golf_course = models.CharField(blank=True, max_length=100, default="")
+    location = models.CharField(blank=True, max_length=255, default="")
+    par = models.IntegerField(null=True)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     week_number = models.CharField(max_length=3)
-    time_zone = models.CharField(max_length=20, null=True, blank=True)
+    time_zone = models.CharField(max_length=20, blank=True, default="")
+    format = models.CharField(max_length=20, default="stroke")
     major_championship = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "tournament"
-
-    def save(self, *args, **kwargs):
-        if not self.id:
-            self.created_at = timezone.now()
-        self.updated_at = timezone.now()
-        return super(Tournament, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name} ({self.year})"
