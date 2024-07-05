79f6d7795ca0e361a08a694709ee06343723ca8c
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-07-05 00:12:18 -0400

    Update comments

--- sgtbackend/call_caddy/services/tournament_call.py
+++ sgtbackend/call_caddy/services/tournament_call.py
@@ -1,3 +1,4 @@
+from models import Tournament
 from datetime import datetime
 from .rapid_api_wrapper import RapidApiWrapperService
 
@@ -7,7 +8,7 @@ class TournamentCallService:
     def __init__(self):
         self.api_wrapper = RapidApiWrapperService()
 
-    # Calls the tour schedule endpoint to GET tourney data for the season
+    # Calls the tournament endpoint to GET data for a specific tourney
     def get_tournament(self, org_id, tourn_id):
         current_year = str(datetime.now().year)
         params = {"orgID": org_id, "tourn_id": tourn_id, "year": current_year}
