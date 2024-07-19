8bc610ad4e9221ae47a9aa15f084fa531191becd
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-07-19 09:05:35 -0400

    Update view and template to show live leaderboard data

--- sgtbackend/grounds_crew/views.py
+++ sgtbackend/grounds_crew/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from call_caddy.services.schedule_call import ScheduleCallService
+from call_caddy.services.leaderboard_call import LeaderboardCallService
 
 # Create your views here.
 
 def login_view(request):
     # Your view logic here
 
-    service_instance = ScheduleCallService()
-    json_string = service_instance.get_schedule(org_id="1")
+    service_instance = LeaderboardCallService()
+    json_string = service_instance.get_leaderboard(org_id="1", tourn_id="100")
     context = {
-        'schedule_data': json_string
+        'leaderboard_data': json_string
         }
     return render(request, 'login.html', context)
